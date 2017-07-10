f664494ab27cfcf0b186e02d1ba86826396de0a8
Mike <michael2.miller@outlook.com>
2017-07-10 16:06:02 +0100

    Update create_model.py

--- create_model.py
+++ create_model.py
@@ -9,8 +9,8 @@ from keras import optimizers
 # dimensions of our images.
 img_width, img_height = 225, 225
 
-top_model_weights_path = '../Python/bottleneck_fc_model.h5'
-train_data_dir = '../Python/data/train/'
+top_model_weights_path = '../Python/bottleneck_fc_model.h5'  # choose where you want to save the weights
+train_data_dir = '../Python/data/train/'                # check these are consistent with create_dataset() directory
 validation_data_dir = '../Python/data/validation/'
 nb_train_samples = 504
 nb_validation_samples = 300
@@ -19,6 +19,7 @@ batch_size = 12
 
 
 def save_bottlebeck_features():
+    # Augment training data and normalise both to be in [0,1] 
     datagen1 = ImageDataGenerator(
         rotation_range=50,
         width_shift_range=0.1,
@@ -29,9 +30,9 @@ def save_bottlebeck_features():
         horizontal_flip=True)
     datagen2 = ImageDataGenerator(rescale=1. / 255)
 
-    # build the VGG16 network
+    # Build the VGG16 network
     model = applications.VGG16(include_top=False, weights='imagenet')
-
+    # Save features
     generator = datagen2.flow_from_directory(
         train_data_dir,
         target_size=(img_width, img_height),
@@ -65,7 +66,7 @@ def train_top_model():
     model.add(Flatten(input_shape=train_data.shape[1:]))
     model.add(Dense(256, activation='relu'))
     model.add(Dropout(0.45))
-    model.add(Dense(1, activation='sigmoid'))
+    model.add(Dense(1, activation='sigmoid')) # Sigmoid for binary classification & Adam's optimiser
     Adam = optimizers.Adamax(lr=0.001, beta_1=0.9, beta_2=0.999,
                              epsilon=1e-08, decay=0.001)
     model.compile(Adam, loss='binary_crossentropy', metrics=['accuracy'])
@@ -76,11 +77,7 @@ def train_top_model():
               validation_data=(validation_data, validation_labels))
     model.save_weights(top_model_weights_path)
 
-
-save_bottlebeck_features()
-train_top_model()
-
-
+# Classify new image with model
 def predict_image(file):
     model = applications.VGG16(include_top=False, weights='imagenet')
     x = image.load_img(file, target_size=(img_width, img_height))
